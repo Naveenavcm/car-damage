59b47fb839edb5c636b2e513bff628a26314242d
Naveena <naveenavcm@gmail.com>
2020-11-20 12:48:34 +0530

    first

--- price_prediction.py
+++ price_prediction.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import numpy as np
-from sklearn.linear_model import LinearRegression
+#from sklearn.linear_model import LinearRegression
 
-from sklearn.model_selection import train_test_split
+#from sklearn.model_selection import train_test_split
 
 import pickle
 
